ce404858972826a2700a9ac3970a6905d6ec0f9f
gorisgor <igor.matvienko1989@gmail.com>
2024-05-30 21:00:39 +0300

    my-project-JSdata

--- src/js/myProjects.js
+++ src/js/myProjects.js
@@ -1,1 +1,92 @@
-//js
+const projectsData = [
+  {
+    mobile:
+      '../img/my-project-images/webwallet mob 1x-min.webp 1x, ../img/my-project-images/webwallet mob 2x-min.webp 2x',
+    tablet:
+      '../img/my-project-images/webwallet tab 1x-min.webp 1x, ../img/my-project-images/webwallet tab 2x-min.webp 2x',
+    desktop:
+      '../img/my-project-images/webwallet pc 1x-min.webp 1x, ../img/my-project-images/webwallet pc 2x-min.webp 2x',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Wallet webservice ',
+    link: 'https://github.com/',
+  },
+  {
+    mobile:
+      '../img/my-project-images/green mob 1x-min.webp 1x, ../img/my-project-images/green mob 2x-min.webp 2x',
+    tablet:
+      '../img/my-project-images/green tab 1x-min.webp 1x, ../img/my-project-images/green tab 2x-min.webp 2x',
+    desktop:
+      '../img/my-project-images/green pc 1x-min.webp 1x, ../img/my-project-images/green pc 2x-min.webp 2x',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Green Harvest online store',
+    link: 'https://github.com/',
+  },
+  {
+    mobile:
+      '../img/my-project-images/english mob 1x-min.webp 1x, ../img/my-project-images/english mob 2x-min.webp 2x',
+    tablet:
+      '../img/my-project-images/english tab 1x-min.webp 1x, ../img/my-project-images/english tab 2x-min.webp 2x',
+    desktop:
+      '../img/my-project-images/english pc 1x-min.webp 1x, ../img/my-project-images/english pc 2x-min.webp 2x',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'English Exellence webservice',
+    link: 'https://github.com/',
+  },
+  {
+    mobile: '',
+    tablet: '',
+    desktop: '',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Power pulse webservice',
+    link: 'https://github.com/',
+  },
+  {
+    mobile: '',
+    tablet: '',
+    desktop: '',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Mimino website',
+    link: 'https://github.com/',
+  },
+  {
+    mobile: '',
+    tablet: '',
+    desktop: '',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Vyshyvanka vibes Landing Page',
+    link: 'https://github.com/',
+  },
+  {
+    mobile: '',
+    tablet: '',
+    desktop: '',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Starlight Studio landing page',
+    link: 'https://github.com/',
+  },
+  {
+    mobile: '',
+    tablet: '',
+    desktop: '',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Chego Jewelry website',
+    link: 'https://github.com/',
+  },
+  {
+    mobile: '',
+    tablet: '',
+    desktop: '',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Energy Flow webservice ',
+    link: 'https://github.com/',
+  },
+  {
+    mobile: '',
+    tablet: '',
+    desktop: '',
+    technologies: 'React, JavaScript, Node JS, Git',
+    description: 'Fruitbox Online store',
+    link: 'https://github.com/',
+  },
+];
+console.log(projectsData);
