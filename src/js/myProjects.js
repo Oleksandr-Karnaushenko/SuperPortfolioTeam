bb425998cbc14b56237fd2479521628f86d3bbe1
gorisgor <igor.matvienko1989@gmail.com>
2024-06-01 10:37:36 +0300

    my-project-JSfunction-update

--- src/js/myProjects.js
+++ src/js/myProjects.js
@@ -1,132 +1,130 @@
 const projectsData = [
   {
     mobile:
-      '../img/my-project-images/webwallet-mob-1x-min.webp 1x, ../img/my-project-images/webwallet-mob-2x-min.webp 2x',
+      '/img/my-project-images/webwallet-mob-1x-min.webp 1x, /img/my-project-images/webwallet-mob-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/webwallet-tab-1x-min.webp 1x, ../img/my-project-images/webwallet-tab-2x-min.webp 2x',
+      '/img/my-project-images/webwallet-tab-1x-min.webp 1x, /img/my-project-images/webwallet-tab-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/webwallet-pc-1x-min.webp 1x, ../img/my-project-images/webwallet-pc-2x-min.webp 2x',
+      '/img/my-project-images/webwallet-pc-1x-min.webp 1x, /img/my-project-images/webwallet-pc-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Wallet webservice ',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/green-mob-1x-min.webp 1x, ../img/my-project-images/green-mob-2x-min.webp 2x',
+      '/img/my-project-images/green-mob-1x-min.webp 1x, /img/my-project-images/green-mob-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/green-tab-1x-min.webp 1x, ../img/my-project-images/green-tab-2x-min.webp 2x',
+      '/img/my-project-images/green-tab-1x-min.webp 1x, /img/my-project-images/green-tab-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/green-pc-1x-min.webp 1x, ../img/my-project-images/green-pc-2x-min.webp 2x',
+      '/img/my-project-images/green-pc-1x-min.webp 1x, /img/my-project-images/green-pc-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Green Harvest online store',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/english-mob-1x-min.webp 1x, ../img/my-project-images/english-mob-2x-min.webp 2x',
+      '/img/my-project-images/english-mob-1x-min.webp 1x, /img/my-project-images/english-mob-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/english-tab-1x-min.webp 1x, ../img/my-project-images/english-tab-2x-min.webp 2x',
+      '/img/my-project-images/english-tab-1x-min.webp 1x, /img/my-project-images/english-tab-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/english-pc-1x-min.webp 1x, ../img/my-project-images/english-pc-2x-min.webp 2x',
+      '/img/my-project-images/english-pc-1x-min.webp 1x, /img/my-project-images/english-pc-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'English Exellence webservice',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/power-1x-min.webp 1x, ../img/my-project-images/power-2x-min.webp 2x',
+      '/img/my-project-images/power-1x-min.webp 1x, /img/my-project-images/power-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/power-1x-min.webp 1x, ../img/my-project-images/power-2x-min.webp 2x',
+      '/img/my-project-images/power-1x-min.webp 1x, /img/my-project-images/power-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/power-1x-min.webp 1x, ../img/my-project-images/power-2x-min.webp 2x',
+      '/img/my-project-images/power-1x-min.webp 1x, /img/my-project-images/power-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Power pulse webservice',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/mimino-1x-min.webp 1x, ../img/my-project-images/mimino-2x-min.webp 2x',
+      '/img/my-project-images/mimino-1x-min.webp 1x, /img/my-project-images/mimino-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/mimino-1x-min.webp 1x, ../img/my-project-images/mimino-2x-min.webp 2x',
+      '/img/my-project-images/mimino-1x-min.webp 1x, /img/my-project-images/mimino-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/mimino-1x-min.webp 1x, ../img/my-project-images/mimino-2x-min.webp 2x',
+      '/img/my-project-images/mimino-1x-min.webp 1x, /img/my-project-images/mimino-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Mimino website',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/vyshyvanka-1x-min.webp 1x, ../img/my-project-images/vyshyvanka-2x-min.webp 2x',
+      '/img/my-project-images/vyshyvanka-1x-min.webp 1x, /img/my-project-images/vyshyvanka-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/vyshyvanka-1x-min.webp 1x, ../img/my-project-images/vyshyvanka-2x-min.webp 2x',
+      '/img/my-project-images/vyshyvanka-1x-min.webp 1x, /img/my-project-images/vyshyvanka-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/vyshyvanka-1x-min.webp 1x, ../img/my-project-images/vyshyvanka-2x-min.webp 2x',
+      '/img/my-project-images/vyshyvanka-1x-min.webp 1x, /img/my-project-images/vyshyvanka-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Vyshyvanka vibes Landing Page',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/studio-1x-min.webp 1x, ../img/my-project-images/studio-2x-min.webp 2x',
+      '/img/my-project-images/studio-1x-min.webp 1x, /img/my-project-images/studio-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/studio-1x-min.webp 1x, ../img/my-project-images/studio-2x-min.webp 2x',
+      '/img/my-project-images/studio-1x-min.webp 1x, /img/my-project-images/studio-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/studio-1x-min.webp 1x, ../img/my-project-images/studio-2x-min.webp 2x',
+      '/img/my-project-images/studio-1x-min.webp 1x, /img/my-project-images/studio-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Starlight Studio landing page',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/jewelry-1x-min.webp 1x, ../img/my-project-images/jewelry-2x-min.webp 2x',
+      '/img/my-project-images/jewelry-1x-min.webp 1x, /img/my-project-images/jewelry-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/jewelry-1x-min.webp 1x, ../img/my-project-images/jewelry-2x-min.webp 2x',
+      '/img/my-project-images/jewelry-1x-min.webp 1x, /img/my-project-images/jewelry-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/jewelry-1x-min.webp 1x, ../img/my-project-images/jewelry-2x-min.webp 2x',
+      '/img/my-project-images/jewelry-1x-min.webp 1x, /img/my-project-images/jewelry-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Chego Jewelry website',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/energy-1x-min.webp 1x, ../img/my-project-images/energy-2x-min.webp 2x',
+      '/img/my-project-images/energy-1x-min.webp 1x, /img/my-project-images/energy-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/energy-1x-min.webp 1x, ../img/my-project-images/energy-2x-min.webp 2x',
+      '/img/my-project-images/energy-1x-min.webp 1x, /img/my-project-images/energy-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/energy-1x-min.webp 1x, ../img/my-project-images/energy-2x-min.webp 2x',
+      '/img/my-project-images/energy-1x-min.webp 1x, /img/my-project-images/energy-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Energy Flow webservice ',
     link: 'https://github.com/',
   },
   {
     mobile:
-      '../img/my-project-images/fruitbox-1x-min.webp 1x, ../img/my-project-images/fruitbox-2x-min.webp 2x',
+      '/img/my-project-images/fruitbox-1x-min.webp 1x, /img/my-project-images/fruitbox-2x-min.webp 2x',
     tablet:
-      '../img/my-project-images/fruitbox-1x-min.webp 1x, ../img/my-project-images/fruitbox-2x-min.webp 2x',
+      '/img/my-project-images/fruitbox-1x-min.webp 1x, /img/my-project-images/fruitbox-2x-min.webp 2x',
     desktop:
-      '../img/my-project-images/fruitbox-1x-min.webp 1x, ../img/my-project-images/fruitbox-2x-min.webp 2x',
+      '/img/my-project-images/fruitbox-1x-min.webp 1x, /img/my-project-images/fruitbox-2x-min.webp 2x',
     technologies: 'React, JavaScript, Node JS, Git',
     description: 'Fruitbox Online store',
     link: 'https://github.com/',
   },
 ];
 
-document.addEventListener('DOMContentLoaded', onSection);
-function onSection() {
-  const projects = document.querySelector('.projects');
-  const loadMoreBtn = document.querySelector('.load-more-btn');
-  const perLoad = 3;
-  let startIndex = 0;
+const projects = document.querySelector('.projects');
+const loadMoreBtn = document.querySelector('.load-more-btn');
+const perLoad = 3;
+let startIndex = 0;
 
-  function renderProjects(start, end) {
-    for (let i = start; i < end; i++) {
-      if (i >= projectsData.length) {
-        return;
-      }
-      const project = projectsData[i];
-      const projectElement = document.createElement('li');
-      projectElement.classList.add('project-card');
-      projectElement.innerHTML = `<picture>
+function renderProjects(start, end) {
+  for (let i = start; i < end; i++) {
+    if (i >= projectsData.length) {
+      return;
+    }
+    const project = projectsData[i];
+    const projectElement = document.createElement('li');
+    projectElement.classList.add('project-card');
+    projectElement.innerHTML = `<picture>
         <source media="(min-width: 1280px)" srcset="${project.desktop}">
         <source media="(min-width: 768px)" srcset="${project.tablet}">
         <source media="(max-width: 767px)" srcset="${project.mobile}">
@@ -135,20 +133,22 @@ function onSection() {
     <div class="project-card-descr">
     <p class="technologies">${project.technologies}</p>
     <h3 class="project-title">${project.description}</h3>
-    <a class="link" href="${project.link}" target="_blank">Visit<svg class="visit-icon" width="24" height="24"><use href="../img/icons.svg#icon-my-projects-visit"></use></svg></a></div>`;
-      projects.appendChild(projectElement);
-    }
+    <a class="link" href="${project.link}" target="_blank">Visit<svg class="visit-icon" width="24" height="24"><use href="/img/icons.svg#icon-my-projects-visit"></use></svg></a></div>`;
+    projects.appendChild(projectElement);
   }
+}
 
-  function loadMoreProjects() {
-    const remainingProjects = projectsData.length - startIndex;
-    const projectsToShow = Math.min(remainingProjects, perLoad);
-    renderProjects(startIndex, startIndex + projectsToShow);
-    startIndex += projectsToShow;
-    if (startIndex >= projectsData.length) {
-      loadMoreBtn.disabled = true;
-    }
+function loadMoreProjects() {
+  const remainingProjects = projectsData.length - startIndex;
+  const projectsToShow = Math.min(remainingProjects, perLoad);
+  renderProjects(startIndex, startIndex + projectsToShow);
+  startIndex += projectsToShow;
+  if (startIndex >= projectsData.length) {
+    loadMoreBtn.disabled = true;
   }
+}
+document.addEventListener('DOMContentLoaded', onSection);
+function onSection() {
   loadMoreBtn.addEventListener('click', loadMoreProjects);
   loadMoreProjects();
 }
