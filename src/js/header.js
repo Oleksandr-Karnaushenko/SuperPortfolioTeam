94d846abdbaa86d29439f20a02b724f0e2f73eb5
AndriyTkh <151773877+AndriyTkh@users.noreply.github.com>
2024-06-01 16:24:02 +0300

    header-overflow

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,17 @@
 //js
+
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.burger-menu');
+
+burgerBtn.addEventListener('click', event => {
+  toggleBurger();
+});
+
+burgerMenu.addEventListener('click', event => {
+  toggleBurger();
+});
+
+function toggleBurger() {
+  burgerBtn.classList.toggle('open');
+  burgerMenu.classList.toggle('hidden');
+}
