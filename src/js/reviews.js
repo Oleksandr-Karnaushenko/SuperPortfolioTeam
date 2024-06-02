3378eee15ae07727438eb31ec72b91ca6485f592
YanaDrozdova <yadro986@gmail.com>
2024-06-02 23:55:42 +0300

    added swiper and js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,1 +1,87 @@
-//js
+// import Swiper JS
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation } from 'swiper/modules';
+import axios from 'axios';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  mousewheel: {
+    invert: true,
+  },
+
+  breakpoints: {
+    1280: {
+      slidesPerView: 2,
+    },
+  },
+
+  spaceBetween: 32,
+});
+
+const nextBtnEl = document.querySelector('.swiper-button-next');
+const prevBtnEL = document.querySelector('.swiper-button-prev');
+axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
+
+async function loadReviewsFromBackend() {
+  try {
+    const response = await axios.get('/reviews');
+    return response.data;
+  } catch (error) {
+    console.error('Failed to load reviews:', error);
+    return [];
+  }
+}
+
+const createReviewsMarkup = reviews => {
+  return reviews
+    .map(
+      review => `<li class="reviews-item swiper-slide">
+        <p class="reviews-text">${review.review}</p>
+        <div class="reviewer-data">
+          <img
+            src="${review.avatar_url}"
+            alt="Natalia Shevchenko"
+            loading="lazy"
+            width="40"
+            height="40"
+            class="reviews-foto"
+          />
+          <p class="reviewer-name">${review.author}</p>
+        </div>
+      </li>`
+    )
+    .join('');
+};
+
+const addReviews = async () => {
+  const reviewsListEL = document.querySelector('.reviews-list');
+  try {
+    const reviews = await loadReviewsFromBackend();
+
+    if (reviews.length === 0) {
+      reviewsListEL.innerHTML = '<p class="not-found">Not found</p>';
+      return;
+    }
+    const markupReviews = createReviewsMarkup(reviews);
+
+    reviewsListEL.innerHTML = markupReviews;
+
+    swiper.update();
+  } catch (error) {
+    console.log(error);
+    alert('Failed to load reviews. Please try again later.');
+  }
+};
+
+window.addEventListener('load', addReviews);
